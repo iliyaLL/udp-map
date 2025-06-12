0a4c7156ee2a62593eab6fa44a9fc85fa73513cb
Iliya <limilia248@gmail.com>
2025-06-12 20:01:19 +0500

    add ping, skeleton set

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,9 +2,44 @@ package server
 
 import (
 	"net"
+	"strings"
 )
 
 func StartServer(port string) error {
+	// Resolve the string address to a UDP address
 	udpAddr, err := net.ResolveUDPAddr("udp", ":"+port)
+	if err != nil {
+		return err
+	}
+
+	// Start listening for UDP packages on the given address
+	conn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	// Read from UDP listener in endless loop
+	for {
+		buf := make([]byte, 1024)
+		_, addr, err := conn.ReadFromUDP(buf)
+		if err != nil {
+			return err
+		}
+
+		command := strings.TrimSpace(string(buf))
+		var response string
+		switch {
+		case strings.HasPrefix(strings.ToLower(command), "ping"):
+			response = "PONG"
+		case strings.HasPrefix(strings.ToLower(command), "set"):
+			response = kvstorage.SET(command)
+		case strings.HasPrefix(strings.ToLower(command), "get"):
+			response = kvstorage.GET(command)
+		default:
+			response = "(error) ERR unknown command"
+		}
+	}
+
 	return nil
 }
--- pkg/kvstorage/kvstorage.go
+++ pkg/kvstorage/kvstorage.go
@@ -1,1 +1,62 @@
 package kvstorage
+
+import (
+	"strings"
+	"sync"
+	"time"
+)
+
+type storageItem struct {
+	value  string
+	expiry time.Time
+}
+
+var (
+	storage = make(map[string]storageItem)
+	mu      sync.RWMutex
+)
+
+// SET Foo Bar
+// SET foo bar px 10000
+func Set(command string) string {
+	parts := strings.Fields(command)
+	if len(parts) < 3 {
+		return "(error) ERR wrong number of arguments for 'SET' command"
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	if len(parts) > 3 && strings.ToUpper(parts[len(parts)-2]) == "PX" {
+		if len(parts) < 5 {
+			return "(error) ERR wrong number of arguments for 'SET' command with PX"
+		}
+
+		// Parse the duration in milliseconds
+		expirationTime, err := time.ParseDuration(parts[len(parts)-1] + "ms")
+		if err != nil {
+			return "(error) ERR invalid PX duration"
+		}
+
+		key, value := parts[1], strings.Join(parts[2:(len(parts)-2)], " ")
+		expiryTime := time.Now().Add(expirationTime)
+		storage[key] = storageItem{
+			value:  value,
+			expiry: expiryTime,
+		}
+	} else {
+		key, value := parts[1], strings.Join(parts[2:], " ")
+		storage[key] = storageItem{
+			value:  value,
+			expiry: time.Time{},
+		}
+	}
+
+	return "OK"
+}
+
+func Get(key string) (string, error) {
+	// This function should retrieve the value associated with the key from the storage.
+	// For now, it returns an empty string and nil error.
+	return "", nil
+}
